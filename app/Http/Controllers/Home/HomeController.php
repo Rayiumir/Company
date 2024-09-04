2932ef15be9bb171114bdb88fa673af528cbbb20
Raymond Baghumian <swordart2050@gmail.com>
2024-09-05 03:27:07 +0330

    Dynamic Services Index Page

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $service = Service::query()->latest()->get();
+        return view('home.index', compact('service'));
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,7 @@
 <x-HomeLayout>
     @include('home.parts.header')
     @include('home.parts.slider')
-    @include('home.parts.service')
+    @include('home.parts.service', ['service' => $service])
     @include('home.parts.portfolio')
     @include('home.parts.posts')
     @include('home.parts.customer')
